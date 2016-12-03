1a001d739f371e0825a2951b6d6fc3afcab46ace
karlobrien <karlobrien@gmail.com>
2016-12-03 09:59:59 +0000

    small changes

---
+++ Day1.py
@@ -0,0 +1,27 @@
+#Build facing Index
+
+# North 0 L: -1, R = 1
+# East 1 L: 1, R = -1
+# South 2 L: 1, R = -1
+# West 3 L: -1, R = 1
+
+input = "L4, R2, R4, L5, L3, L1, R4, R5, R1, R3, L3, L2, L2, R5, R1, L1, L2, R2, R2, L5, R5, R5, L2, R1, R2, L2, L4, L1, R5, R2, R1, R1, L2, L3, R2, L5, L186, L5, L3, R3, L5, R4, R2, L5, R1, R4, L1, L3, R3, R1, L1, R4, R2, L1, L4, R5, L1, R50, L4, R3, R78, R4, R2, L4, R3, L4, R4, L1, R5, L4, R1, L2, R3, L2, R5, R5, L4, L1, L2, R185, L5, R2, R1, L3, R4, L5, R2, R4, L3, R4, L2, L5, R1, R2, L2, L1, L2, R2, L2, R1, L5, L3, L4, L3, L4, L2, L5, L5, R2, L3, L4, R4, R4, R5, L4, L2, R4, L5, R3, R1, L1, R3, L2, R2, R1, R5, L4, R5, L3, R2, R3, R1, R4, L4, R1, R3, L5, L1, L3, R2, R1, R4, L4, R3, L3, R3, R2, L3, L3, R4, L2, R4, L3, L4, R5, R1, L1, R5, R3, R1, R3, R4, L1, R4, R3, R1, L5, L5, L4, R4, R3, L2, R1, R5, L3, R4, R5, L4, L5, R2"
+
+
+coordinates = [{'L': -1, 'R': 1},
+               {'L': 1, 'R': -1},
+               {'L': 1, 'R': -1},
+               {'L': -1, 'R': 1}]
+
+instructions = input.split(', ')
+
+mem = set()
+fi = 0
+x, y = 0, 0
+mem.add((x, y))
+
+for item in instructions:
+    move = item[0]
+    rest = int(item[1:])
+    sign = coordinates[fi][move]
+    print move, ' ', rest, ' ', sign
