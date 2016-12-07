a20bed079d81fbd8261682490497af0a19f557fd
karlobrien <karlobrien@gmail.com>
2016-12-07 07:21:26 +0000

    iter example

--- scratchpad.py
+++ scratchpad.py
@@ -1,4 +1,5 @@
 import sys
+from itertools import izip_longest
 import numpy as np
 import pandas as pd
 
@@ -7,6 +8,13 @@ a = b = c = range(9)
 combine = zip(a,b,c)
 print combine
 
-data = np.loadtxt(input).T.reshape(-1, 3).T
+data = np.array(combine).T.reshape(-1, 3).T
 data.sort(axis=0)
 print data
+
+print '----- iter ----'
+print '--This allow us to iter over a step'
+args = [iter (int(x) for x in a)] * 2
+for x in izip_longest(*args):
+    print x
+
