15a8396cc45ff2d3a94952577f498219191e004f
karlobrien <karlobrien@gmail.com>
2016-12-14 06:26:47 +0000

    Day 9 Part 2 complete

--- Day9/Day9Part2.py
+++ Day9/Day9Part2.py
@@ -3,12 +3,11 @@ import re
 
 file_path = '/Users/karlobrien/projects/python/AdventOfCode2016/Day9/input.txt'
 
-ex2 = '(3x3)XYZ'
-ex3 = 'A(2x2)BCD(2x2)EFG'
-ex4 = '(6x1)(1x3)A'
-ex5 = 'X(8x2)(3x3)ABCY'
+ex1 = '(3x3)XYZ' #XYZXYZXYZ
+ex2 = 'X(8x2)(3x3)ABCY' #XABCABCABCABCABCABCY
+ex3 = '(27x12)(20x12)(13x14)(7x10)(1x12)A' #A repeated 241920 times
+ex4 = '(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN' #445 long
 
-ex1 = 'A(1x5)BC'
 reg = r'\((\d+)x(\d+)\)'
 
 def BreakDown(line):
@@ -21,15 +20,12 @@ def BreakDown(line):
         numberOfItems = int(byRegex.group(1))
         repeat = int(byRegex.group(2))
         first = len(line[:firstPostion])
-        lengthOfRepeatingItems = len(line[lengthOfGroup:lengthOfGroup+numberOfItems] * repeat)
-        segmentLength = first + lengthOfRepeatingItems
-        return segmentLength + BreakDown(line[lengthOfGroup+numberOfItems:])
+        return first + BreakDown(line[lengthOfGroup:lengthOfGroup+numberOfItems]) * repeat + BreakDown(line[lengthOfGroup+numberOfItems:])
 
 print 'Result:',  BreakDown(ex1)
 print 'Result:',  BreakDown(ex2)
 print 'Result:',  BreakDown(ex3)
 print 'Result:',  BreakDown(ex4)
-print 'Result:',  BreakDown(ex5)
 
 r = open(file_path).read()
 print BreakDown(r)
