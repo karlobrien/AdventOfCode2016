2d4d71dfc1316def7e11eaccba8b787eb6ab45e9
karlobrien <karlobrien@gmail.com>
2016-12-21 11:28:31 +0000

    day10

--- Day10/DAy10Part1_2.py
+++ Day10/DAy10Part1_2.py
@@ -1,12 +1,4 @@
-import sys
-import re
 from collections import defaultdict
-
-#only process when you have two processors
-#dict of bot name => tuple of chips
-
-#process and add values checking for two entries
-# if two entries, 
 
 file_name = '/Users/karlobrien/projects/python/AdventOfCode2016/Day10/input.txt'
 
@@ -26,54 +18,6 @@ def parseCmd(entry):
     return cmd[1], cmd[5], cmd[6], cmd[10], cmd[11]
 
 input = open(file_name).read().split('\n')
-items = [ex1, ex2, ex3, ex4, ex5, ex6]
-#input = items
-
-state = defaultdict(list)
-commands = defaultdict(list)
-
-for line in items:
-    if 'value' in line:
-        number, toBot = parseValue(line)
-        #print number, toBot    
-        lenOfToBot = len(state[toBot])
-        if lenOfToBot == 0:
-            state[toBot].append(number)
-        elif lenOfToBot == 1:
-            state[toBot].append(number)
-            sorted(state[toBot])
-        else:
-            #execute command and clear
-            print 'execute command'
-            #botCmds = commands[]
-
-    else:
-        fromBot, lowDesc, lowBot, highDesc, highBot = parseCmd(line)
-        commands[fromBot] = (lowDesc, lowBot, highDesc, highBot)
-
-#print state
-#print commands
-
-allCommands = defaultdict(list)
-for line in input:
-    if 'value' not in line:
-        fromBot, lowDesc, lowBot, highDesc, highBot = parseCmd(line)
-        allCommands[fromBot] = (lowDesc, lowBot, highDesc, highBot)
-
-for line in input:
-    if 'value' in line:
-        number, toBot = parseValue(line)
-
-        lenOfToBot = len(state[toBot])
-        if lenOfToBot == 0:
-            state[toBot].append(number)
-        elif lenOfToBot == 1:
-            state[toBot].append(number)
-            sorted(state[toBot])
-        else:
-            #execute command and clear
-            print 'execute command'
-            #botCmds = commands[]
 
 complete = set()
 myState = defaultdict(list)
@@ -83,28 +27,29 @@ while len(complete) < len(input):
     for line in input:
         if line in complete:
             continue
+
         if 'value' in line:
-            number, toBot = parseValue(line)
+            [number, toBot] = parseValue(line)
             myState[toBot].append(number)
             complete.add(line)
             continue
 
         #parse the command here
         fromBot, lowDesc, lowBot, highDesc, highBot = parseCmd(line)
-        thisBot = myState[fromBot]
 
+        thisBot = myState[fromBot]
         if len(thisBot) < 2:
             continue
 
         currentLBot = min(thisBot)
         currentHBot = max(thisBot)
 
-        if lowDesc == 'output':
+        if lowDesc.startswith('out'):
             output[lowBot].append(currentLBot)
         else:
             myState[lowBot].append(currentLBot)
 
-        if highDesc == 'output':
+        if highDesc.startswith('out'):
             output[highBot].append(currentHBot)
         else:
             myState[highBot].append(currentHBot)
@@ -115,5 +60,3 @@ for x in myState:
     for y in myState[x]:
         print x, y
     print "---"
-
-print output
